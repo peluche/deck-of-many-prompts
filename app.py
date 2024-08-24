241815454b6cda57ee2e37398c90dedba7107690
peluche <me@peluche.me>
2024-08-24 02:27:55 +0200

    history + search + favorite

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # %%
-from fasthtml.common import fast_app, serve, Button, Div, Form, Group, Input, P, Textarea
+from fasthtml.common import fast_app, serve, A, Button, Div, Form, Group, Input, P, Span, Textarea
 from fasthtml.common import *
 import copy
 import base64
@@ -9,23 +9,82 @@ app, rt = fast_app(live=True)
 default_input = 'hi world! :)'
 
 # TODO: replace with immutable datastructure (the clojure kind)
+class Prompt():
+    def __init__(self, prompt, starred=False):
+        self.prompt = prompt
+        self.starred = starred
 world = {}
 world['history'] = {
-    0: 'please be jailbroken',
-    1: 'DAN !',
-    2: 'how to cheat at tic-tac-toe?'
+    0: Prompt('please be jailbroken'),
+    1: Prompt('DAN !'),
+    2: Prompt('how to cheat at tic-tac-toe?', starred=True),
 }
 world['count'] = len(world['history'])
+world['starred_only'] = False
+world['search'] = ''
 old_worlds = []
 
 def prompt(x: str = ''):
     return Textarea(x, id='prompt', name='x', hx_swap_oob='true', style='height: 300px')
 
+@rt('/xoxo')
+def get(): return 'hahaha'
+
+@rt('/history/{id}/star')
+def put(id: int):
+    el = world['history'][id]
+    el.starred = not el.starred
+    return history_el(id)
+
+@rt('/history/star')
+def put():
+    world['starred_only'] = not world['starred_only']
+    return history_list()
+
+def history_el(id: int):
+    el = world['history'][id]
+    return Div(
+        A('❌', hx_delete=f'/history/{id}', hx_target=f'#history-{id}'),
+        A('🌑🌕'[el.starred], hx_put=f'/history/{id}/star', hx_target=f'#history-{id}'),
+        Span(el.prompt, hx_get=f'/xoxo', hx_target=f'#history-{id}'),
+        id=f'history-{id}',
+    )
+
+def filtered_history():
+    return [i for i, el in world['history'].items() if (not world['starred_only'] or el.starred) \
+                                                        and world['search'].lower() in el.prompt.lower()]
+
+def history_list():
+    return Div(*[history_el(i) for i in filtered_history()], id='history')
+
+@rt('/history/search')
+def put(q: str):
+    world['search'] = q
+    return history_list()
+
+@rt('/history')
+def post(x:str):
+    world['history'][world['count']] = Prompt(x)
+    world['count'] += 1
+    return history_list()
+
+def history():
+    return Div(
+        A('🌗', hx_put='/history/star', hx_target='#history'),
+        # TODO: cancel in-flight queries
+        Input(type="search", name='q', value=world['search'], hx_trigger='keyup, search', hx_put='/history/search', hx_target='#history'),
+        history_list(),
+    )
+
 def body(): return Div(
     undo(),
     Form(
         Div(
-            Div(prompt(default_input), style='flex: 0 0 70%'),
+            Div(
+                prompt(default_input),
+                Button('save', hx_post='/history', hx_target='#history'),
+                history(),
+                style='flex: 0 0 70%'),
             Div(
                 Group(Button('b64', hx_post='/b64'), Button('❌', hx_post='/b64d')),
                 Group(Button('morse', hx_post='/morse'), Button('❌', hx_post='/morsed')),
@@ -63,35 +122,23 @@ def get(): return undo()
 # %%
 # history
 
-def history(id: int):
-    x = world['history'][id]
-    return Group(
-        Div(x),
-        Button('🔋', hx_get=f'/load_from_history/{id}'),
-        Button('✏️', hx_get=f'/history/{id}/edit', hx_target=f'#history-{id}'),
-        Button('❌', hx_delete=f'/history/{id}', hx_target=f'#history-{id}'),
-        id=f'history-{id}')
+# @rt('/load_from_history/{id}')
+# def get(id: int):
+#     return Input(id='xxx', name='x', value=world['history'][id], hx_swap_oob='true')
 
-def histories():
-    return Div(*[history(i) for i in world['history']], id='history')
+# @rt('/history/{id}')
+# def get(id: int):
+#     return history(id)
 
-@rt('/load_from_history/{id}')
-def get(id: int):
-    return Input(id='xxx', name='x', value=world['history'][id], hx_swap_oob='true')
-
-@rt('/history/{id}')
-def get(id: int):
-    return history(id)
-
-@rt('/history/{id}/edit')
-def get(id: int):
-    return Group(
-        Form(
-        Input(id=f'history-{id}', name='historyval', value=world['history'][id]),
-        Button('save', hx_put=f'/history/{id}'),
-        Button('cancel', hx_get=f'/history/{id}'),
-        hx_target=f'#history-{id}', hx_swap='outerHTML')
-    )
+# @rt('/history/{id}/edit')
+# def get(id: int):
+#     return Group(
+#         Form(
+#         Input(id=f'history-{id}', name='historyval', value=world['history'][id]),
+#         Button('save', hx_put=f'/history/{id}'),
+#         Button('cancel', hx_get=f'/history/{id}'),
+#         hx_target=f'#history-{id}', hx_swap='outerHTML')
+#     )
 
 @rt('/history/{id}')
 def delete(id: int):
@@ -99,29 +146,29 @@ def delete(id: int):
     del world['history'][id]
     return ''
 
-@rt('/history/{id}')
-def put(id: int, historyval: str):
-    old_worlds.append(copy.deepcopy(world))
-    world['history'][id] = historyval
-    return history(id)
+# @rt('/history/{id}')
+# def put(id: int, historyval: str):
+#     old_worlds.append(copy.deepcopy(world))
+#     world['history'][id] = historyval
+#     return history(id)
 
-@rt('/history')
-def post(x:str):
-    old_worlds.append(copy.deepcopy(world))
-    world['history'][world['count']] = x
-    world['count'] += 1
-    return histories()
+# @rt('/history')
+# def post(x:str):
+#     old_worlds.append(copy.deepcopy(world))
+#     world['history'][world['count']] = x
+#     world['count'] += 1
+#     return histories()
 
-@rt('/history')
-def get(): return Div(
-    histories(),
-    Form(
-        Group(
-            Input(id='xxx', name='x', value=default_input),
-            Button('history', hx_post='/history', hx_target='#history', hx_swap='outerHTML'),
-        ),
-    )
-    )
+# @rt('/history')
+# def get(): return Div(
+#     histories(),
+#     Form(
+#         Group(
+#             Input(id='xxx', name='x', value=default_input),
+#             Button('history', hx_post='/history', hx_target='#history', hx_swap='outerHTML'),
+#         ),
+#     )
+#     )
 
 # %%
 # base64
