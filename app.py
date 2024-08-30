6eb1018eaa14e00bca04b4b80612d7c7df918dd2
peluche <me@peluche.me>
2024-08-31 00:58:50 +0200

    urlencode transform

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import copy
 import base64
 import random
 import string
+import urllib
 from dataclasses import dataclass
 from functools import wraps
 
@@ -171,6 +172,7 @@ def body(): return Div(
                             SGroup(Button('morse', hx_post='/morse'), Button('❌', hx_post='/morsed', cls='xs secondary')),
                             SGroup(Button('ascii', hx_post='/ascii'), Button('❌', hx_post='/asciid', cls='xs secondary')),
                             SGroup(Button('hex', hx_post='/hex'), Button('❌', hx_post='/hexd', cls='xs secondary')),
+                            SGroup(Button('urlencode', hx_post='/urlencode'), Button('❌', hx_post='/urlencoded', cls='xs secondary')),
                             SGroup(Button('binary', hx_post='/binary'), Button('❌', hx_post='/binaryd', cls='xs secondary')),
                             SGroup(Button('rot13', hx_post='/rot13'), Button('❌', hx_post='/rot13', cls='xs secondary')),
                             SGroup(Button('spaces', hx_post='/spaces'), Button('❌', hx_post='/spacesd', cls='xs secondary')),
@@ -651,6 +653,17 @@ def post(x: str):
 def post(x: str):
     decoded, unknown = natod(x) # TODO
     return decoded
+
+# %%
+# urlencode
+
+@rt('/urlencode')
+@handle_selection
+def post(x: str): return urllib.parse.quote(x)
+
+@rt('/urlencoded')
+@handle_selection
+def post(x: str): return urllib.parse.unquote(x)
 
 # %%
 serve()
